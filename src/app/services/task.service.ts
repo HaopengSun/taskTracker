142cfa958cd36b00494a8491077e39830e6f0ba7
HaopengSun <sunhaopeng2016@gmail.com>
2021-06-26 12:47:38 -0400

    interact with mock server using get method in service file

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
-import { TASKS } from '../mock-task'
+// import { TASKS } from '../mock-task'
 import { Task } from '../Task';
 import { Observable, of } from 'rxjs'
+// similar to axios in react to interact with backend server
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl: string = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks =of(TASKS);
-    return tasks
+    // get data from mock data
+    // const tasks =of(TASKS);
+    // return tasks
+
+    // get data from server
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
